d27ddb57f6f3f5ad495e4f325cc438de0fbb3e65
good9527 <good9527@users.noreply.github.com>
2026-05-25 19:05:42 +0800

    fix: 添加缺失的utils.ts工具函数

---
+++ web/src/lib/utils.ts
@@ -0,0 +1,17 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatNumber(num: number): string {
+  if (num >= 10000) {
+    return (num / 10000).toFixed(1) + '万'
+  }
+  return num.toLocaleString()
+}
+
+export function formatPercent(num: number): string {
+  return (num * 100).toFixed(1) + '%'
+}
